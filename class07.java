7a01a65b2b8249119ca81fc6d080ef5bd550e1e7
Yash <yashsontakke03@gmail.com>
2022-12-20 21:52:24 +0530

    uploaded

---
+++ class07.java
@@ -0,0 +1,10 @@
+package Package2;
+import Package1.class05;
+public class class07 {
+    public static void main(String[] args) {
+        class05 ob=new class05();
+        ob.t=100;
+        System.out.println("\n t = " + ob.t);
+        ob.show();
+    }
+}
